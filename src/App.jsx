2d1ca3b3e5438d67c9613bc2e908315a4d8aeaf7
Rafael Almeida Penha <rafaelalmeidapenha@gmail.com>
2022-12-27 16:43:15 -0300

    Estrutura do botões

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { useRef } from "react";
 import "./App.css";
+import { PlayerSlot } from "./components/PlayerSlot/PlayerSlot";
 
 function App() {
+  const modal = useRef();
+  function sequencia(nGolpes, nome) {
+    modal.current.style.display = "block";
+    // const golpes = []
+    // for (let index = 0; index < nGolpes.length; index++) {
+    //   golpes.push(nome)
+    // }
+  }
   return (
     <div className="App">
-      <div className="Arena"></div>
+      <div className="Modal" ref={modal}>
+        <div className="List">sequencia</div>
+        <div className="ModalButton">
+          <div></div>
+          <button className="Button-Cima">CIMA</button>
+          <div></div>
+          <button className="Button-Esquerda">Lados</button>
+          <button className="Button-Meio">Meio</button>
+          <button className="Button-Direta">Lados</button>
+          <div></div>
+          <button className="Button-Baixo">Baixo</button>
+          <div></div>
+        </div>
+      </div>
+      <div className="Arena">
+        <PlayerSlot />
+        <PlayerSlot />
+      </div>
+      <button onClick={sequencia}>TESTE</button>
     </div>
   );
 }
---
+++ src/components/PlayerSlot/PlayerSlot.jsx
@@ -0,0 +1,11 @@
+export function PlayerSlot() {
+  return (
+    <div className="PlayerSlot">
+      <div className="Player-Combo"></div>
+      <div className="Player-Avatar">
+        <img src="" alt="" />
+      </div>
+      <div className="Player-Title">Player 1</div>
+    </div>
+  );
+}
