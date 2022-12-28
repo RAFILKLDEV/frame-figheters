eed41e1f622dba238d7c90fd7dab095e2a756eeb
Rafael Almeida Penha <rafaelalmeidapenha@gmail.com>
2022-12-28 16:42:17 -0300

    Testes com Promise, Testando game

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,110 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 import { PlayerSlot } from "./components/PlayerSlot/PlayerSlot";
 
 function App() {
   const modal = useRef();
-  function sequencia(nGolpes, nome) {
-    modal.current.style.display = "block";
-    // const golpes = []
-    // for (let index = 0; index < nGolpes.length; index++) {
-    //   golpes.push(nome)
-    // }
+  const sprite = [
+    "https://toppng.com/public/uploads/thumbnail/tww-master-sword-espada-destructora-del-mal-11563253029fc7leb3naq.png",
+    "https://emojipedia-us.s3.amazonaws.com/source/skype/289/drop-of-blood_1fa78.png",
+    "https://i.seadn.io/gae/Rg3wJJ6hy-XV1CaJP1lFaFvpBIm2m5li0IlvKI03Tc9C7y0rvZUF_gEfI__fqDNZSp8jtkZyKGxrErThhwN3jSxauMZVFt5DDah9jSU?auto=format&w=1000",
+  ];
+  const [golpe, setGolpe] = useState([]);
+  const [golpe2, setGolpe2] = useState([
+    "Cima",
+    "Cima",
+    "Cima",
+    "Cima",
+    "Cima",
+  ]);
+  const [nGolpe, setNGolpe] = useState(3);
+  const [nGolpe2, setNGolpe2] = useState(3);
+  const [image, setImage] = useState("");
+  const [image2, setImage2] = useState("");
+
+  function golpeButton(nome) {
+    if (nGolpe < 1) {
+      return null;
+    } else {
+      golpe.push(nome);
+      setNGolpe(nGolpe - 1);
+      console.log("Adicionado a " + nome);
+    }
   }
+
   return (
     <div className="App">
       <div className="Modal" ref={modal}>
-        <div className="List">sequencia</div>
+        <div className="List">
+          {" "}
+          {golpe.map((e, i) => (
+            <div key={e + i}>{e}</div>
+          ))}
+        </div>
         <div className="ModalButton">
           <div></div>
-          <button className="Button-Cima">CIMA</button>
+          <button onClick={() => golpeButton("Cima")} className="Button-Cima">
+            CIMA
+          </button>
           <div></div>
-          <button className="Button-Esquerda">Lados</button>
-          <button className="Button-Meio">Meio</button>
-          <button className="Button-Direta">Lados</button>
+          <button
+            onClick={() => golpeButton("Lados")}
+            className="Button-Esquerda"
+          >
+            Lados
+          </button>
+          <button onClick={() => golpeButton("Meio")} className="Button-Meio">
+            Meio
+          </button>
+          <button
+            onClick={() => golpeButton("Lados")}
+            className="Button-Direta"
+          >
+            Lados
+          </button>
           <div></div>
-          <button className="Button-Baixo">Baixo</button>
+          <button onClick={() => golpeButton("Baixo")} className="Button-Baixo">
+            Baixo
+          </button>
           <div></div>
         </div>
       </div>
       <div className="Arena">
-        <PlayerSlot />
-        <PlayerSlot />
+        <PlayerSlot image={image} />
+        <PlayerSlot image={image2} />
       </div>
-      <button onClick={sequencia}>TESTE</button>
+      <button onClick={() => (modal.current.style.display = "block")}>
+        TESTE
+      </button>
+      <button
+        onClick={async () => {
+          const sleep = (m) => new Promise((r) => setTimeout(r, m));
+          await sleep(3000);
+          console.log("kek");
+          await sleep(3000);
+          console.log("kek2");
+        }}
+      >
+        TESTE2
+      </button>
+      <button
+        onClick={async () => {
+          const sleep = (m) => new Promise((r) => setTimeout(r, m));
+          for (let index = 0; index < golpe.length; index++) {
+            await sleep(1000);
+            if (golpe[index] === golpe2[index]) {
+              setImage2(sprite[2]);
+              setImage(sprite[0]);
+              console.log("Defendeu");
+            } else {
+              setImage2(sprite[1]);
+              console.log("Tomou");
+            }
+          }
+        }}
+      >
+        Combate
+      </button>
     </div>
   );
 }
--- src/components/PlayerSlot/PlayerSlot.jsx
+++ src/components/PlayerSlot/PlayerSlot.jsx
@@ -1,9 +1,9 @@
-export function PlayerSlot() {
+export function PlayerSlot(props) {
   return (
     <div className="PlayerSlot">
       <div className="Player-Combo"></div>
       <div className="Player-Avatar">
-        <img src="" alt="" />
+        <img src={props.image} alt="" />
       </div>
       <div className="Player-Title">Player 1</div>
     </div>
